b10041144d6d96b310c04575e7b26a493deebdd6
Vachounet <vachounet@live.fr>
2020-02-12 12:05:50 +0100

    Utils: add FAKE_PACKAGE_SIGNATURE to platform permissions
    Change-Id: Ic00199f10de30c601acb9359ef292de9a150afa3
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
    [kdrag0n: Adapted for Android 11]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- PermissionController/src/com/android/permissioncontroller/permission/utils/Utils.java
+++ PermissionController/src/com/android/permissioncontroller/permission/utils/Utils.java
@@ -21,6 +21,7 @@ import static android.Manifest.permission_group.CALENDAR;
 import static android.Manifest.permission_group.CALL_LOG;
 import static android.Manifest.permission_group.CAMERA;
 import static android.Manifest.permission_group.CONTACTS;
+import static android.Manifest.permission_group.FAKE_PACKAGE;
 import static android.Manifest.permission_group.LOCATION;
 import static android.Manifest.permission_group.MICROPHONE;
 import static android.Manifest.permission_group.NEARBY_DEVICES;
@@ -335,6 +336,8 @@ public final class Utils {
         }
 
         PLATFORM_PERMISSIONS.put(Manifest.permission.BODY_SENSORS, SENSORS);
+
+        PLATFORM_PERMISSIONS.put(Manifest.permission.FAKE_PACKAGE_SIGNATURE, FAKE_PACKAGE);
 
         if (SdkLevel.isAtLeastT()) {
             PLATFORM_PERMISSIONS.put(Manifest.permission.POST_NOTIFICATIONS, NOTIFICATIONS);
