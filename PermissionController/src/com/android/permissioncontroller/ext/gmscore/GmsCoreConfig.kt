922bd91dfb4d4e0e75ada53142c8a4c3c5a200ba
Dmitrii <bankersenator@gmail.com>
2026-04-27 03:58:49 +0300

    Revert "Enable ICC authentication by default (reverse UI logic)"
    This reverts commit 878625686f4f4121291c0e6805c3de6232cff552.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- PermissionController/src/com/android/permissioncontroller/ext/gmscore/GmsCoreConfig.kt
+++ PermissionController/src/com/android/permissioncontroller/ext/gmscore/GmsCoreConfig.kt
@@ -2,19 +2,11 @@ package com.android.permissioncontroller.ext.gmscore
 
 import android.app.compat.gms.GmsCorePackageFlag
 import android.content.pm.ApplicationInfo
-import android.content.pm.GosPackageState
 import android.ext.PackageId
-import android.permission.PermissionManager
-import androidx.annotation.StringRes
-import androidx.appcompat.app.AlertDialog
-import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceGroup
 import androidx.preference.PreferenceScreen
-import androidx.preference.SwitchPreferenceCompat
 import com.android.permissioncontroller.R
 import com.android.permissioncontroller.ext.BaseGosPkgStateConfigFragment
-import android.content.pm.PackageManager
 import com.android.permissioncontroller.ext.BaseSettingsActivity
 import com.android.permissioncontroller.ext.addCategory
 import com.android.permissioncontroller.permission.ui.handheld.PermissionsCollapsingToolbarBaseFragment
@@ -33,78 +25,11 @@ class GmsCoreConfigFragment : BaseGosPkgStateConfigFragment(
 ) {
     override fun configurePreferenceScreen(screen: PreferenceScreen) {
         screen.addCategory(R.string.rcs_activation_category).apply {
-            addPkgFlagPermReversed(
+            addPkgFlagPerm(
                 this, GmsCorePackageFlag.GRANT_PERMS_FOR_ICC_AUTHENTICATION,
                 R.string.gmscore_icc_auth_perms_title,
                 R.string.gmscore_icc_auth_perms_confirm,
             )
-        }
-    }
-
-    /**
-     * Add a package flag permission with REVERSED logic: toggle ON = disable feature, toggle OFF = enable feature.
-     * This makes the feature enabled by default (when flag is not set).
-     */
-    private fun addPkgFlagPermReversed(
-        dst: PreferenceGroup,
-        flag: Int,
-        @StringRes title: Int,
-        @StringRes confirmationText: Int,
-        @StringRes summary: Int = 0
-    ): SwitchPreferenceCompat {
-        val pref = SwitchPreferenceCompat(dst.context)
-        pref.setTitle(title)
-        if (summary != 0) {
-            pref.setSummary(summary)
-        }
-
-        pkgFlagPrefs[flag] = pref
-
-        pref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValueB ->
-            val newValue = newValueB as Boolean
-            val ctx = requireContext()
-
-            if (newValue) {
-                AlertDialog.Builder(ctx).run {
-                    setMessage(getText(confirmationText))
-                    setPositiveButton(R.string.cancel, null)
-                    setNegativeButton(android.R.string.ok) { _, _ ->
-                        updatePackageFlag(flag, true)  // Set flag = disable feature
-                    }
-                    show()
-                }
-                false
-            } else {
-                updatePackageFlag(flag, false)
-               true
-            }
-        }
-
-        dst.addPreference(pref)
-        return pref
-    }
-
-    private fun updatePackageFlag(flag: Int, flagValue: Boolean) {
-        val ctx = requireContext()
-        val userId = android.os.Process.myUserHandle().identifier
-        
-        GosPackageState.edit(packageName, userId).run {
-            setPackageFlagState(flag, flagValue)
-            applyOrPressBack()
-        }
-
-        val permManager = ctx.getSystemService(PermissionManager::class.java)!!
-        permManager.updatePermissionState(packageName, userId)
-
-        GosPackageState.edit(packageName, userId).run {
-            killUidAfterApply()
-            applyOrPressBack()
-        }
-
-        val isPkgEnabled = pkgManager.getApplicationInfo(packageName, 0).enabled
-        if (isPkgEnabled) {
-            pkgManager.setApplicationEnabledSetting(packageName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, userId)
-            pkgManager.setApplicationEnabledSetting(packageName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, userId)
         }
     }
 
